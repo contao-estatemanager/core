8c4d97aa63f05d2c97bd7fb4f6957ca2fcd85f63
oveleon <fabian@oveleon.de>
2019-02-07 14:14:30 +0100

    Routing yaml entry removed

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,19 +10,15 @@
 declare(strict_types=1);
 
 
-
 namespace Oveleon\ContaoImmoManagerBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Oveleon\ContaoImmoManagerBundle\ContaoImmoManagerBundle;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
-class Plugin implements BundlePluginInterface, RoutingPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
@@ -34,16 +30,5 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
                 ->setLoadAfter([ContaoCoreBundle::class])
                 ->setReplace(['immo-manager']),
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
-    {
-        return $resolver
-            ->resolve(__DIR__.'/../Resources/config/routing.yml')
-            ->load(__DIR__.'/../Resources/config/routing.yml')
-            ;
     }
 }
