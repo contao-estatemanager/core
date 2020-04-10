dbb5ee54f87c6deda6ea03a9d736491c60c47cc2
eki89 <fabian@oveleon.de>
2020-04-10 11:58:42 +0200

    Feature to change sorting order added for real estate list modules.

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -21,7 +21,7 @@ array_insert($GLOBALS['TL_DCA']['tl_module']['palettes'], 0, array
 (
     'realEstateExpose'      => '{title_legend},name,headline,type;{config_legend},allowReferences;{module_legend:hide},exposeModules;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID',
     'realEstateFilter'      => '{title_legend},name,headline,type;{include_legend},filter;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID',
-    'realEstateList'        => '{title_legend},name,headline,type;{config_legend},numberOfItems,perPage,hideOnEmpty,listMode;{redirect_legend},jumpTo;{item_extension_legend:hide},addProvider,addContactPerson;{template_legend:hide},statusTokens,customTpl,realEstateTemplate,realEstateProviderTemplate,realEstateContactPersonTemplate;{image_legend:hide},imgSize,providerImgSize,contactPersonImgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,maxTextLength',
+    'realEstateList'        => '{title_legend},name,headline,type;{config_legend},numberOfItems,perPage,hideOnEmpty,listMode;{sorting_legend},listSorting,addCustomOrder;{redirect_legend},jumpTo;{item_extension_legend:hide},addProvider,addContactPerson;{template_legend:hide},statusTokens,customTpl,realEstateTemplate,realEstateProviderTemplate,realEstateContactPersonTemplate;{image_legend:hide},imgSize,providerImgSize,contactPersonImgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,maxTextLength',
     'realEstateResultList'  => '{title_legend},name,headline,type;{config_legend},realEstateGroups,numberOfItems,perPage,filterMode,addCountLabel;{provider_legend},filterByProvider;{sorting_legend},addSorting,addCustomOrder;{redirect_legend},jumpTo;{item_extension_legend:hide},addProvider,addContactPerson;{template_legend:hide},statusTokens,customTpl,realEstateTemplate,realEstateProviderTemplate,realEstateContactPersonTemplate;{image_legend:hide},imgSize,providerImgSize,contactPersonImgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID',
 ));
 
@@ -113,6 +113,16 @@ array_insert($GLOBALS['TL_DCA']['tl_module']['fields'], 1, array
         'inputType'               => 'checkbox',
         'eval'                    => array('tl_class'=>'w50 m12'),
         'sql'                     => "char(1) NOT NULL default ''"
+    ),
+    'listSorting' => array
+    (
+        'label'                   => &$GLOBALS['TL_LANG']['tl_module']['listSorting'],
+        'exclude'                 => true,
+        'inputType'               => 'select',
+        'options'                 => array('none', 'dateAdded_asc', 'dateAdded_desc', 'tstamp_asc', 'tstamp_desc'),
+        'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
+        'eval'                    => array('tl_class'=>'w50'),
+        'sql'                     => "varchar(16) NOT NULL default ''"
     ),
     'addSorting' => array
     (
