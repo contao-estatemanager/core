26eb5de88d217d55da0c5e269cf5bf53bb8d7b47
eki89 <fabian@oveleon.de>
2020-05-15 11:53:58 +0200

    InsertTagsListener implemented as service for insert tags.

---
+++ src/EventListener/InsertTagsListener.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Contao EstateManager.
+ *
+ * @link      https://www.contao-estatemanager.com/
+ * @source    https://github.com/contao-estatemanager/core
+ * @copyright Copyright (c) 2019  Oveleon GbR (https://www.oveleon.de)
+ * @license   https://www.contao-estatemanager.com/lizenzbedingungen.html
+ */
+
+namespace ContaoEstateManager\EstateManager\EventListener;
+
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\Input;
+use ContaoEstateManager\RealEstate;
+use ContaoEstateManager\RealEstateModel;
+
+/**
+ * @internal
+ */
+class InsertTagsListener
+{
+    private const SUPPORTED_TAGS = [
+        'realestate'
+    ];
+
+    /**
+     * @var ContaoFramework
+     */
+    private $framework;
+
+    public function __construct(ContaoFramework $framework)
+    {
+        $this->framework = $framework;
+    }
+
+    /**
+     * @return string|false
+     */
+    public function onReplaceInsertTags(string $tag, bool $useCache, $cacheValue, array $flags)
+    {
+        $elements = explode('::', $tag);
+        $key = strtolower($elements[0]);
+
+        if (\in_array($key, self::SUPPORTED_TAGS, true)) {
+            return $this->replaceRealEstateInsertTags($key, $elements, $flags);
+        }
+
+        return false;
+    }
+
+    private function replaceRealEstateInsertTags(string $insertTag, array $elements, array $flags): string
+    {
+        $this->framework->initialize();
+
+        /** @var RealEstateModel $adapter */
+        $adapter = $this->framework->getAdapter(RealEstateModel::class);
+
+        if (is_numeric($elements[1]))
+        {
+            if (null !== ($model = $adapter->findPublishedByIdOrAlias($elements[1])))
+            {
+                $elements[1] = $elements[2];
+            }
+        }
+        elseif (Input::get('items'))
+        {
+            $model = $adapter->findPublishedByIdOrAlias(Input::get('items'));
+        }
+
+        if ($model === null)
+        {
+            return '';
+        }
+
+        $realEstate = new RealEstate($model);
+
+        switch ($insertTag) {
+            case 'realestate':
+                return $realEstate->{$elements[1]} ?: '';
+        }
+
+        return '';
+    }
+}
