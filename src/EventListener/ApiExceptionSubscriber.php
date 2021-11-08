2d0a74c1bcee7385a4a979f129a6a55d9cfd74a2
doishub <daniele@oveleon.de>
2021-11-08 15:34:51 +0100

    Better error handling for the API

--- src/EventListener/ApiExceptionSubscriber.php
+++ src/EventListener/ApiExceptionSubscriber.php
@@ -8,23 +8,42 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
+/**
+ * Handle api exceptions
+ *
+ * @author Daniele Sciannimanica <https://github.com/doishub>
+ */
 class ApiExceptionSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(RequestEvent $event)
     {
+        $r = $event->getRequest();
         $e = $event->getThrowable();
 
-        if (!$e instanceof ApiBaseException) {
+        if (!$e instanceof ApiBaseException && strpos($r->getPathInfo(), '/api/estatemanager') !== 0) {
             return;
         }
 
-       $response = new JsonResponse([
-           'error'   => 1,
-           'message' => $e->getMessage(),
-           'code'    => $e->getCode()
-       ]);
+        $arrError = [
+            'error'   => 1,
+            'message' => $e->getMessage(),
+            'code'    => $e->getCode()
+        ];
 
-       $event->setResponse($response);
+        // Extend output in case of unforeseeable errors
+        if((strpos($r->getPathInfo(), '/api/') === 0 && !$e instanceof ApiBaseException) || $r->get('debug') == 1)
+        {
+            $arrError['file'] = $e->getFile();
+            $arrError['line'] = $e->getLine();
+        }
+
+        // Extend trace only if debug = 1
+        if($r->get('debug') == 1)
+        {
+            $arrError['trace'] = $e->getTrace();
+        }
+
+       $event->setResponse(new JsonResponse($arrError));
     }
 
     public static function getSubscribedEvents()
