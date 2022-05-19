f925ab0c32728301e08a2f709c5c71d5ab8c4d3f
doishub <daniele@oveleon.de>
2022-05-19 08:56:23 +0200

    Rename HOOK `cemModulePreparation` => `extendTemplateModule`

--- src/Resources/contao/classes/RealEstateModulePreparation.php
+++ src/Resources/contao/classes/RealEstateModulePreparation.php
@@ -40,9 +40,9 @@ class RealEstateModulePreparation extends RealEstate
     public function __call($name, $arguments)
     {
         // HOOK: extend template functions
-        if (isset($GLOBALS['CEM_HOOKS']['cemModulePreparation']) && \is_array($GLOBALS['CEM_HOOKS']['cemModulePreparation']))
+        if (isset($GLOBALS['CEM_HOOKS']['extendTemplateModule']) && \is_array($GLOBALS['CEM_HOOKS']['extendTemplateModule']))
         {
-            foreach ($GLOBALS['CEM_HOOKS']['cemModulePreparation'] as $callback)
+            foreach ($GLOBALS['CEM_HOOKS']['extendTemplateModule'] as $callback)
             {
                 $this->import($callback[0]);
 
