91b60541ab96e9ee8833e84c1e7db43c2e396447
Sebastian Zoglowek <sebastian@oveleon.de>
2022-10-25 16:18:52 +0200

    [Update] Unlock EstateManagerController support for Symfony ^5.4

--- src/Controller/EstateManagerController.php
+++ src/Controller/EstateManagerController.php
@@ -2,25 +2,36 @@
 
 namespace ContaoEstateManager\EstateManager\Controller;
 
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 use ContaoEstateManager\EstateManagerRead;
 
 /**
  * Handles the EstateManager api routes.
  *
+ * @Route(defaults={"_scope" = "frontend"})
+ *
  * @author Daniele Sciannimanica <https://github.com/doishub>
  */
 class EstateManagerController extends AbstractController
 {
+    private ContaoFramework $framework;
+
+    public function __construct(ContaoFramework $framework)
+    {
+        $this->framework = $framework;
+    }
+
     /**
      * Runs the command scheduler. (READ)
      *
-     * @return JsonResponse
+     * @Route("/api/estatemanager/v{version}/{module}/{id}", name="glossary_item_json", defaults={"id" = null})
      */
-    public function readAction($version, $module, $id)
+    public function readAction(int $version, string $module, $id): JsonResponse
     {
-        $this->container->get('contao.framework')->initialize();
+        $this->framework->initialize();
 
         $controller = new EstateManagerRead();
 
